870f73b96c3a9f2cbe1c4a97e7a35585450522a2
Paulo <pc5783346@gmail.com>
2022-09-17 16:17:59 -0300

    feat: token validator creation

---
+++ src/middlewares/tokenValidation.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function tokenValidation(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const { authorization } = req.headers;
+        const token = authorization?.replace("Bearer ", "");
+
+        if(!token) return res.sendStatus(401);
+        
+        jwt.verify(token, process.env.SECRET || "secret", (e, decoded) => {
+            if(e) return res.status(401).send(e.message);
+            
+            res.locals.user = decoded;
+            next()
+        });
+}
